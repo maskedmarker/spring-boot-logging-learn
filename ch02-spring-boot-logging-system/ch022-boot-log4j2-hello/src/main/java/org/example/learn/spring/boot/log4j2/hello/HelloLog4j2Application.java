d13279943f5efdd0a8bf260826dd009fd773f668
maskedmarker <changjianghui0822@gmail.com>
2024-09-24 20:20:07 +0800

    log4j2

---
+++ ch02-spring-boot-logging-system/ch022-boot-log4j2-hello/src/main/java/org/example/learn/spring/boot/log4j2/hello/HelloLog4j2Application.java
@@ -0,0 +1,13 @@
+package org.example.learn.spring.boot.log4j2.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloLog4j2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloLog4j2Application.class, args);
+    }
+
+}
